a4cffaff9631646264d49fcd775bd8230e880869
amanbaghel101 <amanbaghel333@gmail.com>
2024-01-29 12:36:40 +0530

    change sal_vowel

--- sal.sh
+++ sal.sh
@@ -8,6 +8,9 @@ read sal
 if [ $sal -lt 10000 ];
 then
         echo "no tax"
+elif [ $sal -eq 10000 ];
+then
+	echo "2% tax"
 elif [ $sal -gt 1000 ] && [ $sal -lt 25000 ];
 then
         echo "5% tax"
--- vowel.sh
+++ vowel.sh
@@ -5,8 +5,12 @@
 echo "give your character"
 read a
 
-if [ "$a" == "a" ] || [ "$a" == "e" ] || [ "$a" == "i" ] || [ "$a" == "o" ] || [ "$a" == "u" ]; then
+if [ "$a" == "a" ] || [ "$a" == "e" ] || [ "$a" == "i" ] || [ "$a" == "o" ] || [ "$a" == "u" ]; 
+then
     echo "Character is a vowel."
+elif [ "$a" == "A" ] || [ "$a" == "E" ] || [ "$a" == "I" ] || [ "$a" == "O" ] || [ "$a" == "U" ]; 
+then
+        echo " Character is a vowel."
 else
     echo "Character is not a vowel."
 fi
